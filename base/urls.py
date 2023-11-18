914019530f22d6ea57f205030cdfa6fb7aeb04f9
markmark <mmarkmarkm0@gmail.com>
2023-11-18 18:00:32 -0500

    Rendered data to templates from a dictionary

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('home/', views.home, name='home')
+    path('home/', views.home, name='home'),
+    path('room/<int:pk>/', views.room, name='room'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 
+rooms = [
+
+    {"id":1,"name":"DJANGO ROOM"},
+    {"id":2,"name":"WEB DEVELOPMENT"},
+    {"id":3,"name":"BACKEND DEVELOPMENT"},
+    {"id":4,"name":"POLITICS"},
+    {"id":5,"name":"MUSIC"},
+    {"id":6,"name":"SPORTS"},
+
+]
+
 def home(request):
-    return render(request,"home.html")
+    context = {"rooms":rooms}
+    return render(request,"home.html",context)
+
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i["id"] == int(pk):
+            room = i
+            
+    context = {"room":room}
+    return render(request,"room.html",context)
