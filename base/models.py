18d5d456b90266b911ccb0fdc80484174fe31918
markmark <mmarkmarkm0@gmail.com>
2023-11-18 18:43:46 -0500

    Rendered data to templates from the database.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    Room_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Room"
+        db_table = "rooms"
+
+    def __str__(self):
+        return self.Room_name
--- base/views.py
+++ base/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
-
+from .models import Room
 
 rooms = [
 
-    {"id":1,"name":"DJANGO ROOM"},
+    {"id":1,"name":"DJANGO"},
     {"id":2,"name":"WEB DEVELOPMENT"},
     {"id":3,"name":"BACKEND DEVELOPMENT"},
     {"id":4,"name":"POLITICS"},
     {"id":5,"name":"MUSIC"},
     {"id":6,"name":"SPORTS"},
+    {"id":7,"name":"GAMING"},
+
 
 ]
 
 def home(request):
+
+    rooms = Room.objects.all()
     context = {"rooms":rooms}
     return render(request,"home.html",context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i["id"] == int(pk):
-            room = i
-            
+
+    room = Room.objects.get(id=pk)
     context = {"room":room}
     return render(request,"room.html",context)
