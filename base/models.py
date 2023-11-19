2a8c9628368716623e2c68c04f826959a5854bd0
markmark <mmarkmarkm0@gmail.com>
2023-11-18 19:01:20 -0500

    Added more models

--- base/models.py
+++ base/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Topic(models.Model):
+    Topic_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Topic_name
+    
 class Room(models.Model):
+    Host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    Topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     Room_name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
+    #participants = 
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -12,3 +22,13 @@ class Room(models.Model):
 
     def __str__(self):
         return self.Room_name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[:50]
